9bd581af26d239ea7e4d7254ad4f7924d42b75ea
Thiago P. Bueno <thiago.pbueno@gmail.com>
2017-08-05 13:40:38 -0300

    Dump post title and url to json

--- sakabot/spider.py
+++ sakabot/spider.py
@@ -1,9 +1,11 @@
 import scrapy
+import json
 
 class Sakabot(scrapy.Spider):
 
 	name = 'Sakabot'
 	start_urls = ['https://blogdosakamoto.blogosfera.uol.com.br/']
+	posts_links = []
 
 	def parse(self, response):
 		POST_SELECTOR = '#conteudo-principal .post.news'
@@ -11,10 +13,14 @@ class Sakabot(scrapy.Spider):
 		for article in response.css(POST_SELECTOR):
 			TITLE_SELECTOR = 'a ::text'
 			URL_SELECTOR = 'a ::attr(href)'
-			yield {
+			post =  {
 				'title': article.css(TITLE_SELECTOR).extract_first(),
 				'url': article.css(URL_SELECTOR).extract_first(),
 			}
+			Sakabot.posts_links.append(post)
+			yield post
+
+		self._dump_posts()
 
 		PREVIOUS_SELECTOR = '.blog-navigation .previous-post ::attr(href)'
 		previous_page = response.css(PREVIOUS_SELECTOR).extract_first()
@@ -24,3 +30,7 @@ class Sakabot(scrapy.Spider):
                 response.urljoin(previous_page),
                 callback=self.parse
             )
+
+	def _dump_posts(self):
+		with open('posts.json', 'w') as outfile:
+			json.dump(Sakabot.posts_links, outfile, sort_keys=True, indent=4)
